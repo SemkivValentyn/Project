891490b79f507db7e6985b1356ad3eb1310a03f2
Valentyn <semkiv.valentyn@gmail.com>
2022-02-09 13:14:45 +0200

    first task

--- js/script.js
+++ js/script.js
@@ -12,8 +12,11 @@ const personalMovieDB = {
 };
 
 const a = prompt("Що ви в останній раз дивилися?", ""),
-      b = prompt("На скільки оцінюєте його?"),
-      c = prompt("Що ви в останній раз дивилися?", ""),
-      d = prompt("На скільки оцінюєте його?");
+    b = prompt("На скільки оцінюєте його?"),
+    c = prompt("Що ви в останній раз дивилися?", ""),
+    d = prompt("На скільки оцінюєте його?");
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
