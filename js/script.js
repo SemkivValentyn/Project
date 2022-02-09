85a0c93e64562792c36d374d1d937a96bdb3cd48
Valentyn <semkiv.valentyn@gmail.com>
2022-02-09 16:04:55 +0200

    second task

--- js/script.js
+++ js/script.js
@@ -13,12 +13,39 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("Що ви в останній раз дивилися?", ""),
-    b = prompt("На скільки оцінюєте його?"),
-    c = prompt("Що ви в останній раз дивилися?", ""),
-    d = prompt("На скільки оцінюєте його?");
+// const a = prompt("Що ви в останній раз дивилися?", ""),
+//     b = prompt("На скільки оцінюєте його?"),
+//     c = prompt("Що ви в останній раз дивилися?", ""),
+//     d = prompt("На скільки оцінюєте його?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+//TODO: Secpnd task
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Що ви в останній раз дивилися?", ""),
+        b = +prompt("На скільки оцінюєте його?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("Err");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Доволі мало");
+} else if (personalMovieDB >= 10 && personalMovieDB < 30) {
+    console.log("Не погано");
+} else if (personalMovieDB >= 30) {
+    console.log("Ви кіноман");
+} else {
+    console.log("Помилка");
+}
+
 
 console.log(personalMovieDB);
