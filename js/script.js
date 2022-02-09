6aea392a9b43055d97815542760928a2ab70db50
Valentyn <semkiv.valentyn@gmail.com>
2022-02-09 13:03:14 +0200

    first task

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+
+const numberOfFilms = +prompt("Скільки фільмів подивилися?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Що ви в останній раз дивилися?", ""),
+      b = prompt("На скільки оцінюєте його?"),
+      c = prompt("Що ви в останній раз дивилися?", ""),
+      d = prompt("На скільки оцінюєте його?");
+
+
